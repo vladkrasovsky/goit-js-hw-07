15e67b19d7ae51092ac3ef82cc8a1d54eb7660ce
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-09-15 22:35:49 +0300

    Adds 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map(createGalleryMarkup).join("");
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+refs.gallery.innerHTML = galleryMarkup;
+refs.gallery.addEventListener("click", handleModalOpen);
+
+function handleModalOpen(e) {
+  e.preventDefault();
+  const { nodeName, dataset } = e.target;
+  nodeName === "IMG" && showModal(dataset.source);
+}
+
+function showModal(src) {
+  const handleModalClose = ({ code }) => {
+    code === "Escape" && modal.close();
+  };
+
+  const modalMarkup = `<img src="${src}" width="800" height="600">`;
+
+  const modalOptions = {
+    onShow: () => {
+      document.addEventListener("keydown", handleModalClose);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", handleModalClose);
+    },
+  };
+
+  const modal = basicLightbox.create(modalMarkup, modalOptions);
+
+  modal.show();
+}
+
+function createGalleryMarkup({ preview, original, description }) {
+  return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+  `;
+}
