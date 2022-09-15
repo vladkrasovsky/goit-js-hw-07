3f059b67ca203513da4281cab7def654b21d6787
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-09-15 22:48:02 +0300

    Adds 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map(createGalleryMarkup).join("");
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+refs.gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryMarkup({ preview, original, description }) {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  `;
+}
